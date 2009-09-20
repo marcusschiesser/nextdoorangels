1db56ed200f6ffa4942312e9ead67e36b83cfac7
Marcus.Schiesser <Marcus.Schiesser@87acefc0-a5ef-11de-afd7-c7ca60a6e2ec>
2009-09-20 18:11:48 +0000

    initial view

---
+++ trunk/nextdoorangels/application/controllers/ProjectController.php
@@ -0,0 +1,19 @@
+<?php 
+$err = error_reporting(E_ERROR);
+require_once 'facebook/facebook.php';
+error_reporting($err);
+
+class ProjectController extends Zend_Controller_Action {
+
+    public function init() {
+        /* Initialize action controller here */
+    }
+    
+    function indexAction() {
+        
+        
+    }
+
+    
+}
+
