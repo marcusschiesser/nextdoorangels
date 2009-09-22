c74f6dd06b18aab4dba6a9a7740c7a031e0f181f
Marcus.Schiesser <Marcus.Schiesser@87acefc0-a5ef-11de-afd7-c7ca60a6e2ec>
2009-09-22 16:17:01 +0000

    - used static maps for preview - started controller code

--- trunk/nextdoorangels/application/controllers/ProjectController.php
+++ trunk/nextdoorangels/application/controllers/ProjectController.php
@@ -9,18 +9,29 @@ class ProjectController extends Zend_Controller_Action {
         /* Initialize action controller here */
     }
     
-    function indexAction() {
-        
-        
+    public function addAction() {
+    	$request = $this->getRequest();
+        if ($request->isPost()) {
+        	// get values
+        	$title = $request->getParam('title');
+			$description = $request->getParam('description');
+			$place = $request->getParam('place');
+			$deadline_day = $request->getParam('deadline_day');
+			$deadline_month = $request->getParam('deadline_month');
+			$send_invitation = $request->getParam('send_invitation');
+			// validate values
+        	$this->view->errors = array();
+			$titleValidator = new Zend_Validate_StringLength(8);
+			if (!$titleValidator->isValid($title)) {
+				$this->view->errors['title'] = current($titleValidator->getMessages());
+			}
+            // do the commit
+			if(count($this->view->errors) == 0) {
+				// TODO: inform user
+				return $this->_forward('index');
+			}
+        } 
     }
-
-	function addAction() {
-		
-	}
 	
-	function showmapAction() {
-		
-	}
-    
 }
 
