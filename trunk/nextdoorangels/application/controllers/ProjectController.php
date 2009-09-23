1a1e15853b9b36c0938877e42427d41bcc335ce3
Marcus.Schiesser <Marcus.Schiesser@87acefc0-a5ef-11de-afd7-c7ca60a6e2ec>
2009-09-23 13:19:41 +0000

    - validatoren hinzugefügt - flash message hinzugefügt (über view helper)

--- trunk/nextdoorangels/application/controllers/ProjectController.php
+++ trunk/nextdoorangels/application/controllers/ProjectController.php
@@ -10,28 +10,41 @@ class ProjectController extends Zend_Controller_Action {
     }
     
     public function addAction() {
-    	$request = $this->getRequest();
-        if ($request->isPost()) {
-        	// get values
-        	$title = $request->getParam('title');
-			$description = $request->getParam('description');
-			$place = $request->getParam('place');
-			$deadline_day = $request->getParam('deadline_day');
-			$deadline_month = $request->getParam('deadline_month');
-			$send_invitation = $request->getParam('send_invitation');
-			// validate values
-        	$this->view->errors = array();
-			$titleValidator = new Zend_Validate_StringLength(8);
-			if (!$titleValidator->isValid($title)) {
-				$this->view->errors['title'] = current($titleValidator->getMessages());
-			}
+        $this->view->errors = array();
+        $request = $this->getRequest();
+        $doit = $request->getParam('do-it');
+        if (isset($doit)) {
+            // get values
+            $title = $request->getParam('title');
+            $description = $request->getParam('description');
+            $place = $request->getParam('place');
+            $deadline_day = $request->getParam('deadline_day');
+            $deadline_month = $request->getParam('deadline_month');
+            $send_invitation = $request->getParam('send_invitation');
+            // validate values
+            $titleValidator = new Zend_Validate();
+            $titleValidator->addValidator( new Zend_Validate_StringLength(8))->addValidator( new Zend_Validate_Alnum());
+            if (!$titleValidator->isValid($title)) {
+                $this->view->errors['Title'] = current($titleValidator->getMessages());
+            }
+            $descriptionValidator = new Zend_Validate_StringLength(8);
+            if (!$descriptionValidator->isValid($description)) {
+                $this->view->errors['Description'] = current($descriptionValidator->getMessages());
+            }
+            $placeValidator = new Zend_Validate_StringLength(3);
+            if (!$placeValidator->isValid($place)) {
+                $this->view->errors['Place'] = current($placeValidator->getMessages());
+            }
             // do the commit
-			if(count($this->view->errors) == 0) {
-				// TODO: inform user
-				return $this->_forward('index');
-			}
-        } 
+            if (count($this->view->errors) == 0) {
+                // commit project
+                
+                // TODO: inform user
+				$this->_helper->FlashMessenger('You successfully created a social project. We wish you a lot of success.');
+                return $this->_forward('index', 'index');
+            }
+        }
     }
-	
+    
 }
 
