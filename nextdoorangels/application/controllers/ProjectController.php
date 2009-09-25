d0d331811651eaedec9cc502ac6c9daadee8afeb
Marcus.Schiesser <Marcus.Schiesser@87acefc0-a5ef-11de-afd7-c7ca60a6e2ec>
2009-09-25 12:18:03 +0000

    deadline wird nun automatisch auf 2 monate vom aktuellen datum an gesetzt

--- nextdoorangels/application/controllers/ProjectController.php
+++ nextdoorangels/application/controllers/ProjectController.php
@@ -16,19 +16,17 @@ class ProjectController extends FacebookController {
         $this->view->errors = array();
         $request = $this->getRequest();
         $doit = $request->getParam('do-it');
-		// check permission
-		$this->view->permission = $this->facebook->api_client->users_hasAppPermission('create_event'); 
+        // check permission
+        $this->view->permission = $this->facebook->api_client->users_hasAppPermission('create_event');
         if (isset($doit) && $this->view->permission) {
             // get values
             $title = $request->getParam('title');
             $description = $request->getParam('description');
             $place = $request->getParam('place');
-            $deadline_day = $request->getParam('deadline_day');
-            $deadline_month = $request->getParam('deadline_month');
-            $deadline = mktime(23, 59, 59, $deadline_month, $deadline_day);
-            if ($deadline == FALSE) {
-                $this->view->errors['Deadline'] = 'is not a valid date.';
-            }
+			$starttime = new Zend_Date();
+            $deadline = new Zend_Date();
+            // set $deadline by adding 2 months from now on
+            $deadline->addMonth(2);
             // validate values
             $titleValidator = new Zend_Validate();
             $titleValidator->addValidator( new Zend_Validate_StringLength(8));
@@ -48,19 +46,18 @@ class ProjectController extends FacebookController {
                 // do geo lookup
                 list($lng, $lat) = $this->lookupAdress($place);
                 // commit project
-				$event_data = array('name'=>$title, 'city'=>$place, 'start_time'=>time(), 'end_time'=>$deadline,
-				'category' => 1, 'subcategory' => 1, 'location' => 'Your House', 'host' => 'You');
+                $event_data = array('name'=>$title, 'city'=>$place, 'start_time'=>$starttime->getTimestamp(), 'end_time'=>$deadline->getTimestamp(), 'category'=>1, 'subcategory'=>1, 'location'=>'Your House', 'host'=>'You');
                 try {
                     $event_id = $this->facebook->api_client->events_create($event_data);
-	                $table = new Model_DbTable_Problems();
-	                $table->insert(array('p_name'=>$title, 'p_description'=>$description, 'p_address'=>$place, 'p_lat'=>$lat, 'p_lng'=>$lng, 'fb_user_id'=>$this->fbUserId, 'p_deadline'=>date("Y-m-d H:i:s", $deadline), 'p_created_at'=>date("Y-m-d H:i:s", time()), 'fb_event_id'=>$event_id));
-	                // inform user & forward to index
-	                $this->_helper->FlashMessenger('You successfully created the social project <fb:eventlink eid="'.$event_id.'"/>. Just click on the link and invite some friends. We wish you a lot of success. ');
+                    $table = new Model_DbTable_Problems();
+                    $table->insert(array('p_name'=>$title, 'p_description'=>$description, 'p_address'=>$place, 'p_lat'=>$lat, 'p_lng'=>$lng, 'fb_user_id'=>$this->fbUserId, 'p_deadline'=>$deadline->toString('YYYY-MM-dd HH:mm:ss'), 'p_created_at'=>$starttime->toString('YYYY-MM-dd HH:mm:ss'), 'fb_event_id'=>$event_id));
+                    // inform user & forward to index
+                    $this->_helper->FlashMessenger('You successfully created the social project <fb:eventlink eid="'.$event_id.'"/>. Just click on the link and invite some friends. We wish you a lot of success. ');
                 }
                 catch(Exception $e) {
-	                $this->_helper->FlashMessenger(array('error'=>'There has been an error creating your social project. Please try again later.'));
-					Zend_Registry::get('logger')->err($e->getMessage()); 
-                } 
+                    $this->_helper->FlashMessenger(array('error'=>'There has been an error creating your social project. Please try again later.'));
+                    Zend_Registry::get('logger')->err($e->getMessage());
+                }
                 return $this->_forward('index', 'index');
             }
         }
