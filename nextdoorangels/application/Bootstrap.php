469005b646c0073122195ccd5f7032cd213d2b7f
Marcus.Schiesser <Marcus.Schiesser@87acefc0-a5ef-11de-afd7-c7ca60a6e2ec>
2009-09-25 10:44:52 +0000

    added application logging

--- nextdoorangels/application/Bootstrap.php
+++ nextdoorangels/application/Bootstrap.php
@@ -1,26 +1,25 @@
-<?php
-
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
-	protected function _initAutoload()
-	{
-		$moduleLoader = new Zend_Application_Module_Autoloader(array(
-			'namespace' => '',
-			'basePath' => APPLICATION_PATH));
-		return $moduleLoader;
-	}
-	
-	function _initViewHelpers()
-  	{
-       $this->bootstrap('layout');
-       $layout = $this->getResource('layout');
-       $view = $layout->getView();
-
+<?php 
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
+    protected function _initAutoload() {
+        $moduleLoader = new Zend_Application_Module_Autoloader(array('namespace'=>'', 'basePath'=>APPLICATION_PATH));
+        
+        $writer = new Zend_Log_Writer_Stream(APPLICATION_PATH . '/../public/app.log');
+        $logger = new Zend_Log($writer);
+        Zend_Registry::set('logger', $logger);
+        
+        return $moduleLoader;
+    }
     
-       $view->doctype('XHTML1_STRICT');
-       $view->headMeta()->appendHttpEquiv('Content-Type', 'text/html;charset=utf-8');
-       $view->headTitle()->setSeparator(' - ');
-       $view->headTitle('Hoopz');
-  }
+    function _initViewHelpers() {
+        $this->bootstrap('layout');
+        $layout = $this->getResource('layout');
+        $view = $layout->getView();
+
+        
+        $view->doctype('XHTML1_STRICT');
+        $view->headMeta()->appendHttpEquiv('Content-Type', 'text/html;charset=utf-8');
+        $view->headTitle()->setSeparator(' - ');
+        $view->headTitle('Hoopz');
+    }
 }
 
