c7e4b6b2384f686f7cad6066ac0aa4d2d1ca136a
Marcus.Schiesser <Marcus.Schiesser@87acefc0-a5ef-11de-afd7-c7ca60a6e2ec>
2009-10-05 15:07:32 +0000

    - added map directly to project (with own google maps api key) - modified controller so language is delegated to view

--- trunk/nextdoorangels/application/controllers/InfoController.php
+++ trunk/nextdoorangels/application/controllers/InfoController.php
@@ -9,7 +9,8 @@ class InfoController extends FacebookController {
 		$this->view->mapRoot = $allConfig['map']['root'];
 		
 		$this->view->userId = $this->fbUserId;
-		
+		$userInfo = $this->facebook->api_client->users_getInfo( $this->fbUserId, 'locale' );
+		$this->view->language = substr($userInfo[0]['locale'], 0, 2);
     }
 	
 	public function aboutAction() {
---
+++ trunk/nextdoorangels/public/map/map.php
@@ -0,0 +1,18 @@
+<?php header('P3P:CP="IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT"'); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>NextdoorAngels - map</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf8"/>
+        <link rel="stylesheet" type="text/css" href="http://www.geowidget.de/geowidget-js/css/style.css" />
+		<script type="text/javascript" src="http://yui.yahooapis.com/3.0.0/build/yui/yui-min.js"></script>
+		<script type="text/javascript">
+		    YUI.namespace("geowidget");
+			YUI.geowidget.root = 'data';
+		</script>
+        <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=ABQIAAAAquIIHMFUJg94ExRueMgLfBRqIoZm6jji5rsO5B8qBiDUbrl1FRQdaeL1jsj3fTRyvOT7EK7euL9jmA" type="text/javascript"></script>
+        <script type="text/javascript" src="http://www.geowidget.de/geowidget-js/build/geowidget.js"></script>
+    </head>
+	
+    <body class="yui-skin-sam form" onunload="GUnload();"></body>
+</html>
