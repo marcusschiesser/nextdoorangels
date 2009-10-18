6d4f171f321997c82765e80df920ed8401f5be2f
Marcus.Schiesser <Marcus.Schiesser@87acefc0-a5ef-11de-afd7-c7ca60a6e2ec>
2009-10-18 22:09:46 +0000

    added time and add for a project -> turns into an event

--- trunk/nextdoorangels/application/controllers/ProjectController.php
+++ trunk/nextdoorangels/application/controllers/ProjectController.php
@@ -17,7 +17,7 @@ class ProjectController extends FacebookController {
         //Zend_Registry::get('logger')->debug("response: ".print_r($response, true));
         return array($response->Placemark[0]->Point->coordinates[0], $response->Placemark[0]->Point->coordinates[1]);
     }
-	
+    
     public function addAction() {
         $this->requireLogin();
         $this->view->errors = array();
@@ -31,11 +31,22 @@ class ProjectController extends FacebookController {
             $description = $request->getParam('description');
             $street = $request->getParam('street');
             $city = $request->getParam('city');
-			$this->view->params = $request->getParams();
-            $starttime = new Zend_Date();
-            $deadline = new Zend_Date();
-            // set $deadline by adding 2 months from now on
-            $deadline->addMonth(2);
+            $this->view->params = $request->getParams();
+			date_default_timezone_set('Europe/Berlin');
+            $createdTime = new Zend_Date();
+            $startTime = new Zend_Date();
+            $startTime->set($request->getParam('date_month'), Zend_Date::MONTH);
+            $startTime->set($request->getParam('date_day'), Zend_Date::DAY);
+            $startTime->set($request->getParam('time_hour'), Zend_Date::HOUR_SHORT_AM);
+            $startTime->set($request->getParam('time_min'), Zend_Date::MINUTE);
+            if ($request->getParam('time_ampm') == 'pm') {
+                $startTime->add(12, Zend_Date::HOUR);
+            }
+			$startTime->setTimezone('America/Los_Angeles');
+            $endTime = clone $startTime;
+            $endTime->add(4, Zend_Date::HOUR);
+			$this->view->date = $startTime->getTimestamp() + $startTime->getGmtOffset(); 
+			// TODO: convert FB timezone http://forum.developers.facebook.com/viewtopic.php?id=24220
             // validate values
             $titleValidator = new Zend_Validate();
             $titleValidator->addValidator( new Zend_Validate_StringLength(8));
@@ -63,23 +74,23 @@ class ProjectController extends FacebookController {
                 if (count($this->view->errors) == 0) {
                     try {
                         // commit project
-                        $event_data = array('name'=>$title, 'city'=>$city, 'location'=>$street, 'start_time'=>$starttime->getTimestamp(), 'end_time'=>$deadline->getTimestamp(), 'category'=>2, 'subcategory'=>30, 'host'=>'You');
+                        $event_data = array('name'=>$title, 'city'=>$city, 'location'=>$street, 'start_time'=>$startTime->getTimestamp(), 'end_time'=>$endTime->getTimestamp(), 'category'=>2, 'subcategory'=>30, 'host'=>'You');
                         $event_id = $this->facebook->api_client->events_create($event_data);
                         $table = new Model_DbTable_Problems();
-                        $table->insert(array('p_name'=>$title, 'p_description'=>$description, 'p_city'=>$city, 'p_location'=>$street, 'p_lat'=>$lat, 'p_lng'=>$lng, 'fb_user_id'=>$this->fbUserId, 'p_deadline'=>$deadline->toString('YYYY-MM-dd HH:mm:ss'), 'p_created_at'=>$starttime->toString('YYYY-MM-dd HH:mm:ss'), 'fb_event_id'=>$event_id));
+                        $table->insert(array('p_name'=>$title, 'p_description'=>$description, 'p_city'=>$city, 'p_location'=>$street, 'p_lat'=>$lat, 'p_lng'=>$lng, 'fb_user_id'=>$this->fbUserId, 'p_deadline'=>$startTime->toString('YYYY-MM-dd HH:mm:ss'), 'p_created_at'=>$createdTime->toString('YYYY-MM-dd HH:mm:ss'), 'fb_event_id'=>$event_id));
                         // inform user & forward to index
                         $this->_helper->FlashMessenger('You successfully created the social project <fb:eventlink eid="'.$event_id.'"/>. Just click on the link and invite some friends. We wish you a lot of success. ');
                     }
                     catch(Exception $e) {
-						if($e->getMessage()=='Unknown city') {
-			                $this->view->errors['City'] = ': ' . $city . ' is unknown in Facebook. Please try a different name.';
-							return;
-						} elseif(strpos($e->getMessage(),'Integrity constraint violation')!==FALSE) {
-                        	$this->_helper->FlashMessenger(array('error'=>'You tried to create a project at '.$street.', '.$city.' - the same location of an existing project. Probably you just reloaded the page, and everything is fine. If not, please create a project at another location.'));
-						} else {
-                        	$this->_helper->FlashMessenger(array('error'=>'There has been an error creating your social project. Please try again later.'));
-                        	Zend_Registry::get('logger')->err($e->getMessage());
-						}
+                        if ($e->getMessage() == 'Unknown city') {
+                            $this->view->errors['City'] = ': '.$city.' is unknown in Facebook. Please try a different name.';
+                            return;
+                        } elseif (strpos($e->getMessage(), 'Integrity constraint violation') !== FALSE) {
+                            $this->_helper->FlashMessenger(array('error'=>'You tried to create a project at '.$street.', '.$city.' - the same location of an existing project. Probably you just reloaded the page, and everything is fine. If not, please create a project at another location.'));
+                        } else {
+                            $this->_helper->FlashMessenger(array('error'=>'There has been an error creating your social project. Please try again later.'));
+                            Zend_Registry::get('logger')->err($e->getMessage());
+                        }
                     }
                     return $this->_forward('index', 'index');
                 }
@@ -87,27 +98,18 @@ class ProjectController extends FacebookController {
         }
     }
     
-	public function listAction() {
-		$this->_helper->Layout->disableLayout();
+    public function listAction() {
+        $this->_helper->Layout->disableLayout();
         $this->_helper->ViewRenderer->setNoRender();
         $table = new Model_DbTable_Problems();
         $rows = $table->fetchAll();
-		$values = array();
+        $values = array();
         foreach ($rows as $row) {
-        	$value = array("templates" => array("{root}/templates/fb.html"), "icon" => "slp",
-            "city" => htmlspecialchars($row['p_city']),
-            "location" => htmlspecialchars($row['p_location']),
-            "address" => htmlspecialchars($row['p_location'].', '.$row['p_city']),
-            "projectTitle" => htmlspecialchars($row['p_name']),
-            "description" => nl2br(htmlspecialchars($row['p_description'])),
-            "userId" => $row['fb_user_id'],
-            "joinURL" => "javascript:window.top.location = 'http://www.facebook.com/event.php?eid=".$row['fb_event_id']."';",
-            "lat" => $row['p_lat'],
-            "lng" => $row['p_lng']);
-			array_push($values, $value);
+            $value = array("templates"=>array("{root}/templates/fb.html"), "icon"=>"slp", "city"=>htmlspecialchars($row['p_city']), "location"=>htmlspecialchars($row['p_location']), "address"=>htmlspecialchars($row['p_location'].', '.$row['p_city']), "projectTitle"=>htmlspecialchars($row['p_name']), "description"=>nl2br(htmlspecialchars($row['p_description'])), "userId"=>$row['fb_user_id'], "joinURL"=>"javascript:window.top.location = 'http://www.facebook.com/event.php?eid=".$row['fb_event_id']."';", "lat"=>$row['p_lat'], "lng"=>$row['p_lng']);
+            array_push($values, $value);
         }
         $output = $_GET['callback'].'('.Zend_Json::encode($values).');';
         $this->_response->setHeader('Content-Type', 'text/plain')->setBody($output);
-	}
+    }
 }
 
